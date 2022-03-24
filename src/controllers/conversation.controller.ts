d7064a7e5a65afe9daa244824a00dc9201b760bc
maurosaravia <mauro.saravia@rootstrap.com>
2022-03-24 11:49:20 -0300

    Refactoring conversation list
    - Corrected identation
    - Added async to ConversationsService methods
    - Followed the single responsability principle and added a existsConversation method
    - Renamed variables and functions

--- src/controllers/conversation.controller.ts
+++ src/controllers/conversation.controller.ts
@@ -1,22 +1,21 @@
 import {
-    JsonController,
-    Get,
-    Authorized,
-    CurrentUser,
-  } from 'routing-controllers';
-  import { Service } from 'typedi';
+  JsonController,
+  Get,
+  Authorized,
+  CurrentUser
+} from 'routing-controllers';
+import { Service } from 'typedi';
 import { ConversationsService } from '@services/conversation.service';
 import { Conversation } from '@entities/conversation.entity';
-  
-  @JsonController('/conversations')
-  @Service()
-  export class ConversationController {
-    constructor(private readonly conversationsService: ConversationsService) {}
-  
-    @Authorized()
-    @Get()
-    async index(@CurrentUser() user :number): Promise<Conversation[]> {
-      return this.conversationsService.listConversationsByUser(user);
-    }
+
+@JsonController('/conversations')
+@Service()
+export class ConversationController {
+  constructor(private readonly conversationsService: ConversationsService) {}
+
+  @Authorized()
+  @Get()
+  async getConversations(@CurrentUser() userId :number): Promise<Conversation[]> {
+    return this.conversationsService.listConversationsByUser(userId);
   }
-  
+}
--- src/services/conversation.service.ts
+++ src/services/conversation.service.ts
@@ -1,20 +1,24 @@
 import { Service } from 'typedi';
 import { getRepository } from 'typeorm';
 import { Conversation } from '@entities/conversation.entity';
-// import { ConversationInterface } from '@interfaces';
 
 @Service()
 export class ConversationsService {
   private readonly conversationRepository = getRepository<Conversation>(Conversation);
 
-  listConversationsByUser(userId: number) {
-    return this.conversationRepository.find({ where: [{ user1Id: userId }, { user2Id: userId }] });
+  async listConversationsByUser(userId: number) {
+    return this.conversationRepository.find({ where: [{ user1Id: userId },
+      { user2Id: userId }] });
   }
 
   async createConversation(conversation: Conversation) {
-    const exists = (await this.conversationRepository.count({ where:
+    return await this.existsConversation(conversation) ?
+      this.conversationRepository.save(conversation) : null;
+  }
+
+  async existsConversation(conversation: Conversation) {
+    return await this.conversationRepository.count({ where:
       [{ user1Id: conversation.user1Id, user2Id: conversation.user2Id },
-        { user1Id: conversation.user2Id, user2Id: conversation.user1Id }] })) > 0;
-    return !exists? this.conversationRepository.save(conversation) : null;
+        { user1Id: conversation.user2Id, user2Id: conversation.user1Id }] }) > 0;
   }
 }
