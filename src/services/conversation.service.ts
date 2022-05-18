18f0a56589205f2981d6918a40f01eb7feaf867e
maurosaravia <mauro.saravia@rootstrap.com>
2022-05-18 14:38:35 -0300

    refactor ConversationDTO

--- src/services/conversation.service.ts
+++ src/services/conversation.service.ts
@@ -1,4 +1,4 @@
-import Container, { Service } from 'typedi';
+import { Container, Service } from 'typedi';
 import { getRepository } from 'typeorm';
 import { Conversation } from '@entities/conversation.entity';
 import { ConversationDTO } from '@dto/conversationDTO';
@@ -9,34 +9,34 @@ import { MessagesService } from './message.service';
 export class ConversationsService {
   private readonly conversationRepository = getRepository<Conversation>(Conversation);
 
-  async listConversations() {
+  async listConversations(): Promise<ConversationDTO[]> {
     const result = await this.conversationRepository.find();
     return this.completeConversations(result);
   }
 
-  async listConversationsByUser(userId: number) {
+  async listConversationsByUser(userId: number): Promise<ConversationDTO[]> {
     const result = await this.conversationRepository.find({ where: [{ user1Id: userId },
       { user2Id: userId }] });
     return this.completeConversations(result);
   }
 
-  async createConversation(conversation: Conversation) {
+  async createConversation(conversation: Conversation): Promise<Conversation | null> {
     return !await this.existsConversation(conversation) ?
       this.conversationRepository.save(conversation) : null;
   }
 
-  async existsConversation(conversation: Conversation) {
+  async existsConversation(conversation: Conversation): Promise<boolean> {
     return await this.conversationRepository.count({ where:
       [{ user1Id: conversation.user1Id, user2Id: conversation.user2Id },
         { user1Id: conversation.user2Id, user2Id: conversation.user1Id }] }) > 0;
   }
 
-  async isUserInConversation(userId: number, conversationId: number) {
+  async isUserInConversation(userId: number, conversationId: number): Promise<boolean> {
     const conversation = await this.conversationRepository.findOne({ where:
        { id: conversationId } });
     return conversation?.user1Id === userId || conversation?.user2Id === userId;
   }
-  async completeConversations(conversations: Conversation[]) {
+  async completeConversations(conversations: Conversation[]): Promise<ConversationDTO[]> {
     const result: ConversationDTO[] = [];
     const messagesService = Container.get(MessagesService);
     for (const c of conversations) {
--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -8,7 +8,7 @@ import { Conversation } from '@entities/conversation.entity';
 export class MessagesService {
   private readonly messageRepository = getRepository<Message>(Message);
 
-  async sendMessage(message: Message) {
+  async sendMessage(message: Message): Promise<Message | null> {
     const conversationService = Container.get(ConversationsService);
     const conversation = new Conversation();
     conversation.user1Id = message.senderId;
@@ -17,12 +17,14 @@ export class MessagesService {
       this.messageRepository.save(message) : null;
   }
 
-  async getMessages(conversationId: number, page: number, pageLength: number) {
+  async getMessages(conversationId: number, page: number,
+    pageLength: number): Promise<Message[]> {
     return this.messageRepository.find({ where: { conversationId: conversationId },
       skip: (page - 1) * pageLength, take: pageLength, order: { createdAt: 'DESC' } });
   }
 
-  async readMessages(conversationId: number, userId: number, readDate: Date) {
+  async readMessages(conversationId: number, userId: number,
+    readDate: Date): Promise<void> {
     this.messageRepository.createQueryBuilder().update(Message).set({ readAt: readDate })
       .where('conversationId = :conversationId', { conversationId: conversationId })
       .andWhere('receiverId != :userId', { userId: userId })
@@ -30,7 +32,7 @@ export class MessagesService {
       .andWhere('createdAt < :readDate', { readDate: readDate })
       .execute();
   }
-  async unreadCount(conversationId: number) {
+  async unreadCount(conversationId: number): Promise<number> {
     return this.messageRepository.count({ where: { conversationId: conversationId,
       readAt: null } });
   }
